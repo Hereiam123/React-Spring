7cec6124e8e9319532d04bb2ef9de7a0408b41bd
hereiam123 <bdemaio1@gmail.com>
2019-03-03 23:57:04 -0500

    Added second component

--- src/components/Component2.js
+++ src/components/Component2.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
+import { Spring } from "react-spring/renderprops";
 
 class Component2 extends Component {
   render() {
-    return <div />;
+    return (
+      <Spring
+        from={{ opacity: 0 }}
+        to={{ opacity: 1 }}
+        config={{ delay: 500, duration: 500 }}
+      >
+        {props => (
+          <div style={props}>
+            <div style={c2Style}>
+              <h1>Component 2</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                hic. Aliquam incidunt quasi architecto! Repellendus neque,
+                voluptatum explicabo ad qui, animi placeat ducimus consequuntur
+                nesciunt perferendis corporis illum atque dolorem.
+              </p>
+            </div>
+          </div>
+        )}
+      </Spring>
+    );
   }
 }
+
+const c2Style = {
+  background: "slateblue",
+  color: "white",
+  padding: "1.5rem"
+};
 
 export default Component2;
